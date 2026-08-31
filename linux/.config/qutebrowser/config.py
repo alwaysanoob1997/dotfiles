315639fd5f43cc790fcd18de9eaca684d1ccc751
Kevin Chacko <kevinpc1997@gmail.com>
2026-08-31 07:05:26 +0530

    orphan binding removed from qute config

--- linux/.config/qutebrowser/config.py
+++ linux/.config/qutebrowser/config.py
@@ -11,7 +11,6 @@ c.editor.command = [
 config.bind(",b", "spawn --userscript qute-bitwarden-search-rbw -n")
 config.bind(",p", "spawn --userscript qute-bitwarden-search-rbw -wn")
 config.bind(",u", "spawn --userscript qute-bitwarden-search-rbw -en")
-config.bind(",t", "spawn --userscript qute-bitwarden")
 
 config.bind("co", "set content.proxy socks://127.0.0.1:9050")
 config.bind("cO", "set content.proxy system")
