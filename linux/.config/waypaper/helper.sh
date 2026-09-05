f87896ef6ea7d0010fd56c6d9e1ffd9b3ee7ce11
Kevin Chacko <kevinpc1997@gmail.com>
2026-09-05 23:36:42 +0530

    added waypaper changes

---
+++ linux/.config/waypaper/helper.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+WALLPAPER="$1"
+CACHE="$HOME/.cache/wallpaper"
+THUMB="$CACHE/current-thumb.jpg"
+
+mkdir -p "$CACHE"
+
+# Generate thumbnail asynchronously
+(
+  TEMP="$CACHE/current-thumb.tmp.jpg"
+
+  ffmpeg -y -i "$WALLPAPER" \
+    -vf "scale=800:-1" \
+    -q:v 5 \
+    "$TEMP" 2>/dev/null
+
+  mv "$TEMP" "$THUMB"
+) &
+
+# Continue immediately with Matugen
+matugen image "$WALLPAPER" -m dark --source-color-index 0
