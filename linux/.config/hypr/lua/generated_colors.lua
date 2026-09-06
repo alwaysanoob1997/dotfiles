2f1cd997dd2f39da914c308ff8e22f0c2aafd707
Kevin Chacko <kevinpc1997@gmail.com>
2026-09-06 15:27:09 +0530

    minor regression in matugen fixed

--- linux/.config/hypr/lua/generated_colors.lua
+++ linux/.config/hypr/lua/generated_colors.lua
@@ -1,12 +1,12 @@
 return {
 
-	background = "rgb(19, 19, 24)",
-	on_background = "rgb(228, 225, 233)",
-	outline = "rgb(145, 143, 154)",
-	outline_variant = "rgb(70, 70, 79)",
-	primary = "rgb(191, 194, 255)",
-	on_primary = "rgb(39, 43, 96)",
-	secondary = "rgb(197, 196, 221)",
-	on_secondary = "rgb(46, 47, 66)",
+	background = "rgb(25, 17, 18)",
+	on_background = "rgb(240, 222, 223)",
+	outline = "rgb(159, 140, 142)",
+	outline_variant = "rgb(82, 67, 69)",
+	primary = "rgb(255, 178, 189)",
+	on_primary = "rgb(86, 29, 41)",
+	secondary = "rgb(229, 189, 193)",
+	on_secondary = "rgb(67, 41, 45)",
 	shadow = "rgb(0, 0, 0)",
 }
