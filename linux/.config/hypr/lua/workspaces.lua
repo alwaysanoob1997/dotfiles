fc0d0d625caa14fc3838098307d9b04df9dbfa6a
Kevin Chacko <kevinpc1997@gmail.com>
2026-09-06 11:22:25 +0530

    hyprland full screen cchange

--- linux/.config/hypr/lua/workspaces.lua
+++ linux/.config/hypr/lua/workspaces.lua
@@ -8,27 +8,27 @@
 -- Example window rules that are useful
 
 local suppressMaximizeRule = hl.window_rule({
-    -- Ignore maximize requests from all apps. You'll probably like this.
-    name  = "suppress-maximize-events",
-    match = { class = "org.qutebrowser.qutebrowser" },
+	-- Ignore maximize requests from all apps. You'll probably like this.
+	name = "suppress-maximize-events",
+	match = { class = "org.qutebrowser.qutebrowser" },
 
-    suppress_event = "maximize",
+	suppress_event = "maximize",
 })
--- suppressMaximizeRule:set_enabled(false)
+suppressMaximizeRule:set_enabled(true)
 
 hl.window_rule({
-    -- Fix some dragging issues with XWayland
-    name  = "fix-xwayland-drags",
-    match = {
-        class      = "^$",
-        title      = "^$",
-        xwayland   = true,
-        float      = true,
-        fullscreen = false,
-        pin        = false,
-    },
+	-- Fix some dragging issues with XWayland
+	name = "fix-xwayland-drags",
+	match = {
+		class = "^$",
+		title = "^$",
+		xwayland = true,
+		float = true,
+		fullscreen = false,
+		pin = false,
+	},
 
-    no_focus = true,
+	no_focus = true,
 })
 
 -- Layer rules also return a handle.
@@ -41,41 +41,38 @@ hl.window_rule({
 
 -- Hyprland-run windowrule
 hl.window_rule({
-    name  = "move-hyprland-run",
-    match = { class = "hyprland-run" },
+	name = "move-hyprland-run",
+	match = { class = "hyprland-run" },
 
-    move  = "20 monitor_h-120",
-    float = true,
+	move = "20 monitor_h-120",
+	float = true,
 })
 
 hl.window_rule({
 	workspace = "6",
-	match = {class = "^(steam_app_default)$"},
+	match = { class = "^(steam_app_default)$" },
 })
 
 -- Supposed to avoid idling of full screen games
 hl.window_rule({
 	match = {
-	    class = "^(steam_app_default)$", 
-            fullscreen = true,  
-    },
-        idle_inhibit = "fullscreen",
+		class = "^(steam_app_default)$",
+		fullscreen = true,
+	},
+	idle_inhibit = "fullscreen",
 })
-
-
 
 local acer = "DP-5"
 local virtual = "VIRTUAL-1"
 
-hl.workspace_rule({ workspace = "1", monitor = acer, default=true})
-hl.workspace_rule({ workspace = "2", monitor = acer})
-hl.workspace_rule({ workspace = "3", monitor = acer})
-hl.workspace_rule({ workspace = "4", monitor = acer})
-hl.workspace_rule({ workspace = "5", monitor = acer})
+hl.workspace_rule({ workspace = "1", monitor = acer, default = true })
+hl.workspace_rule({ workspace = "2", monitor = acer })
+hl.workspace_rule({ workspace = "3", monitor = acer })
+hl.workspace_rule({ workspace = "4", monitor = acer })
+hl.workspace_rule({ workspace = "5", monitor = acer })
 
-hl.workspace_rule({ workspace = "6", monitor = virtual, default=true})
-hl.workspace_rule({ workspace = "7", monitor = virtual})
-hl.workspace_rule({ workspace = "8", monitor = virtual})
-hl.workspace_rule({ workspace = "9", monitor = virtual})
-hl.workspace_rule({ workspace = "10", monitor = virtual})
-
+hl.workspace_rule({ workspace = "6", monitor = virtual, default = true })
+hl.workspace_rule({ workspace = "7", monitor = virtual })
+hl.workspace_rule({ workspace = "8", monitor = virtual })
+hl.workspace_rule({ workspace = "9", monitor = virtual })
+hl.workspace_rule({ workspace = "10", monitor = virtual })
