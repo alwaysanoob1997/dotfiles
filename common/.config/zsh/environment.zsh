ca4a8ebfb119e7d45edb0b8470f809544e42172c
Kevin Chacko <kevinpc1997@gmail.com>
2026-09-19 11:40:25 +0530

    added .local/bin to Path

--- common/.config/zsh/environment.zsh
+++ common/.config/zsh/environment.zsh
@@ -2,7 +2,7 @@
 export EDITOR="nvim"
 
 export STARSHIP_CONFIG="$HOME/.config/starship/starship.toml"
-
+export PATH="$HOME/.local/bin:$PATH"
 
 export FZF_DEFAULT_COMMAND='fd --hidden --strip-cwd-prefix --exclude .git'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
